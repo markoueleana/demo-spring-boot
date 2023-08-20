b1a07f571ed37173f50f072c17f9e268e26687ae
eleana.mark <Toshiba@DESKTOP-HNKGUJ6>
2023-08-20 21:08:47 +0300

    add Employee entity class

---
+++ 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/springboot/cruddemo/entity/Employee.java
@@ -0,0 +1,70 @@
+package com.springboot.cruddemo.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="employee")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private int id;
+
+    @Column(name="first_name")
+    private int firstName;
+
+    @Column(name="last_name")
+    private int lastName;
+    @Column(name="email")
+    private int email;
+    public Employee(){
+
+    }
+    public Employee(int firstName, int lastName, int email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(int firstName) {
+        this.firstName = firstName;
+    }
+
+    public int getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(int lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getEmail() {
+        return email;
+    }
+
+    public void setEmail(int email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", firstName=" + firstName +
+                ", lastName=" + lastName +
+                ", email=" + email +
+                '}';
+    }
+}
