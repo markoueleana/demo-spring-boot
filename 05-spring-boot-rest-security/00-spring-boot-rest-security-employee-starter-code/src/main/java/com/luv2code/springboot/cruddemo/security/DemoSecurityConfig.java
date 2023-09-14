7e8d91079f284de727dd92cfbe537da948c0d60f
eleana.mark <Toshiba@DESKTOP-HNKGUJ6>
2023-09-14 21:38:38 +0300

    Jdbc Authentication

--- 05-spring-boot-rest-security/00-spring-boot-rest-security-employee-starter-code/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
+++ 05-spring-boot-rest-security/00-spring-boot-rest-security-employee-starter-code/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
@@ -8,35 +8,18 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class DemoSecurityConfig {
-
     @Bean
-    public InMemoryUserDetailsManager userDetailsManager() {
-
-        UserDetails giannis = User.builder()
-                .username("Giannis")
-                .password("{noop}test123")
-                .roles("ADMIN")
-                .build();
-
-        UserDetails billy = User.builder()
-                .username("Billy")
-                .password("{noop}test123")
-                .roles("EMPLOYEE","MANAGER")
-                .build();
-
-        UserDetails tasos = User.builder()
-                .username("Tasos")
-                .password("{noop}test123")
-                .roles("EMPLOYEE","MANAGER","ADMIN")
-                .build();
-        return new InMemoryUserDetailsManager(giannis, billy, tasos);
+    public UserDetailsManager userDetailsManager(DataSource datasource){
+        return new JdbcUserDetailsManager(datasource);
     }
-
-
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(configurer ->
