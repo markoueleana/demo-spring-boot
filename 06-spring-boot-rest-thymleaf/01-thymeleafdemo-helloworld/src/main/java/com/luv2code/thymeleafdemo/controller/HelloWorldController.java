a72324c4e8495430bedcf26aef33b73956d7d2bc
eleana.mark <Toshiba@DESKTOP-HNKGUJ6>
2023-10-11 23:24:28 +0300

    HelloWorldController name display

---
+++ 06-spring-boot-rest-thymleaf/01-thymeleafdemo-helloworld/src/main/java/com/luv2code/thymeleafdemo/controller/HelloWorldController.java
@@ -0,0 +1,39 @@
+package com.luv2code.thymeleafdemo.controller;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloWorldController {
+
+
+      @RequestMapping("/showForm")
+    public String showFrom(){
+        return "helloworld-form";
+    }
+
+
+    @RequestMapping("/processForm")
+    public String processForm(){
+        return "helloworld";
+    }
+
+    @RequestMapping("/processForm-V2")
+    public String processFormV2(HttpServletRequest request, Model model){
+        String name = request.getParameter("studentName");
+          model.addAttribute("message","The is the name "+ name);
+          return "helloworld";
+    }
+
+    @RequestMapping("/processForm-V3")
+    public String processFormV3(@RequestParam ("studentName") String theName, Model model){
+
+        model.addAttribute("message","The is the name "+ theName);
+        return "helloworld";
+    }
+}
