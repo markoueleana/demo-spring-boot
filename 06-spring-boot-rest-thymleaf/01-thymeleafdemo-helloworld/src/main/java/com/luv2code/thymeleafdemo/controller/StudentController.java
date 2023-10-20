4c4ae70c60a901b57b1ac5323bea8dab93c1673d
eleana.mark <Toshiba@DESKTOP-HNKGUJ6>
2023-10-20 22:23:23 +0300

    add countries dynamically

--- 06-spring-boot-rest-thymleaf/01-thymeleafdemo-helloworld/src/main/java/com/luv2code/thymeleafdemo/controller/StudentController.java
+++ 06-spring-boot-rest-thymleaf/01-thymeleafdemo-helloworld/src/main/java/com/luv2code/thymeleafdemo/controller/StudentController.java
@@ -1,17 +1,22 @@
 package com.luv2code.thymeleafdemo.controller;
 
 import com.luv2code.thymeleafdemo.student.Student;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @Controller
 public class StudentController {
-
+@Value("${countries}")
+private List<String> countries;
 @GetMapping("/showStudentForm")
     public String showForm(Model model){
         Student theStudent = new Student();
         model.addAttribute("student",theStudent);
+    model.addAttribute("countries",countries);
         return "student-form";
     }
     @PostMapping("/processStudentForm")
