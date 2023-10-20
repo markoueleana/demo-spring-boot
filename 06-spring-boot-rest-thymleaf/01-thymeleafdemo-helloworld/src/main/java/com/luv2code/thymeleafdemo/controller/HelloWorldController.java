b82b2aaf93029cda50b44977884f1f06c87e9ccc
eleana.mark <Toshiba@DESKTOP-HNKGUJ6>
2023-10-20 20:05:27 +0300

    pass attributes to the form

--- 06-spring-boot-rest-thymleaf/01-thymeleafdemo-helloworld/src/main/java/com/luv2code/thymeleafdemo/controller/HelloWorldController.java
+++ 06-spring-boot-rest-thymleaf/01-thymeleafdemo-helloworld/src/main/java/com/luv2code/thymeleafdemo/controller/HelloWorldController.java
@@ -5,6 +5,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -12,24 +13,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class HelloWorldController {
 
 
-      @RequestMapping("/showForm")
+    @GetMapping("/showForm")
     public String showFrom(){
         return "helloworld-form";
     }
 
 
-    @RequestMapping("/processForm")
-    public String processForm(){
-        return "helloworld";
-    }
-
-    @RequestMapping("/processForm-V2")
-    public String processFormV2(@RequestParam("studentName") String name , Model model){
-          model.addAttribute("message","The is the name "+ name);
-          return "helloworld";
-    }
-
-    @RequestMapping("/processForm-V3")
+    @PostMapping("/processForm-V3")
     public String processFormV3(@RequestParam ("studentName") String theName, Model model){
 
         model.addAttribute("message","The is the name "+ theName);
