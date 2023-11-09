2cefd9388679d4909fbb3a472faada8bc9ea1aa5
eleana.mark <Toshiba@DESKTOP-HNKGUJ6>
2023-11-09 23:08:07 +0200

    Mix and Max annotation validation

--- 06-spring-boot-rest-thymleaf/validationdemo/src/main/java/com/luv2code/springdemo/mvc/Customer.java
+++ 06-spring-boot-rest-thymleaf/validationdemo/src/main/java/com/luv2code/springdemo/mvc/Customer.java
@@ -1,5 +1,7 @@
 package com.luv2code.springdemo.mvc;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
@@ -9,6 +11,17 @@ public class Customer {
     @NotNull(message = "is required")
     @Size(min = 1, message = "is required")
     private String lastName;
+    @Min(value = 17, message = "Ooops, looks like you are younger than expected!")
+    @Max(value = 100, message = "Ooops, looks like you are older than expected!")
+    private int age;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 
     public String getFirstName() {
         return firstName;
