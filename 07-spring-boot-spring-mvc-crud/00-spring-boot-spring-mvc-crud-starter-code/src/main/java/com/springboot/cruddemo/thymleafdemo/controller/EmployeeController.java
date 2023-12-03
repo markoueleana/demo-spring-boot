e94743969294869ccf635be67ba98ccbcdd1a1e0
eleana.mark <Toshiba@DESKTOP-HNKGUJ6>
2023-12-03 20:42:15 +0200

    Add Update and Delete action buttons

--- 07-spring-boot-spring-mvc-crud/00-spring-boot-spring-mvc-crud-starter-code/src/main/java/com/springboot/cruddemo/thymleafdemo/controller/EmployeeController.java
+++ 07-spring-boot-spring-mvc-crud/00-spring-boot-spring-mvc-crud-starter-code/src/main/java/com/springboot/cruddemo/thymleafdemo/controller/EmployeeController.java
@@ -4,10 +4,7 @@ import com.springboot.cruddemo.thymleafdemo.entity.Employee;
 import com.springboot.cruddemo.thymleafdemo.service.EmployeeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -27,10 +24,23 @@ public class EmployeeController {
         return "employees/list-employees";
     }
     @GetMapping("/showFromForAdd")
-    public String addEmploye(Model theModel){
+    public String addEmployee(Model theModel){
         Employee theEmployees =new Employee();
         theModel.addAttribute("employee",theEmployees);
         return "employees/add-employee";
+    }
+    @GetMapping("/showFromForUpdate")
+    public String updateEmployee(@RequestParam("employeeId") int employeeId, Model theModel){
+        Employee theEmployees =  employeeService.findById(employeeId);
+
+        theModel.addAttribute("employee",theEmployees);
+        return "employees/update-employee";
+    }
+    @GetMapping("/delete")
+    public String deleteEmployee(@RequestParam("employeeId") int employeeId, Model theModel){
+        Employee theEmployees =  employeeService.findById(employeeId);
+        employeeService.delete(theEmployees);
+        return "redirect:/employees/list";
     }
     @PostMapping("/save")
     public String saveEmployee(@ModelAttribute("employee") Employee theNewEmployee){
