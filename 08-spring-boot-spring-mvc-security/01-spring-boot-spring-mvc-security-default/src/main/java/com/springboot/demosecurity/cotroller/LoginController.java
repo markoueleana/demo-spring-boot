cff104e3f3b9e37f4c7ae5670966be5cd5127621
eleana.mark <Toshiba@DESKTOP-HNKGUJ6>
2023-12-10 18:37:47 +0200

    custom login page

--- 08-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/src/main/java/com/springboot/demosecurity/cotroller/LoginController.java
+++ 08-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/src/main/java/com/springboot/demosecurity/cotroller/LoginController.java
@@ -4,10 +4,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class DemoController {
+public class LoginController {
 
-    @GetMapping("/")
-    public String showHome(){
-        return "home";
+
+    @GetMapping("/showMyLoginPage")
+    public String showLoginPage(){
+        return "login-page";
     }
+
+
 }
--- 08-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/src/main/java/com/springboot/demosecurity/security/DemoSecurityConfig.java
+++ 08-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/src/main/java/com/springboot/demosecurity/security/DemoSecurityConfig.java
@@ -2,13 +2,14 @@ package com.springboot.demosecurity.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class DemoSecurityConfig {
-
     @Bean
     public InMemoryUserDetailsManager userDetailsManager() {
 
@@ -31,5 +32,18 @@ public class DemoSecurityConfig {
                 .build();
 
         return new InMemoryUserDetailsManager(peli, lenia, susan);
+    }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(configurer->
+                configurer
+                        .anyRequest().authenticated()
+        ).formLogin(form->
+                form
+                        .loginPage("/showMyLoginPage")
+                        .loginProcessingUrl("/authenticateUser")
+                        .permitAll()
+        );
+        return http.build();
     }
 }
