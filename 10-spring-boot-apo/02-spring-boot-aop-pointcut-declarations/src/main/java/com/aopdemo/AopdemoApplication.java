180c4e4ad4b23347f20bd5194089a6e47fb1b605
eleana.mark <Toshiba@DESKTOP-HNKGUJ6>
2024-02-07 20:14:49 +0200

    pointcut declarations

---
+++ 10-spring-boot-apo/02-spring-boot-aop-pointcut-declarations/src/main/java/com/aopdemo/AopdemoApplication.java
@@ -0,0 +1,31 @@
+package com.aopdemo;
+
+import com.aopdemo.dao.AccountDAO;
+import com.aopdemo.dao.MembershipDAO;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class AopdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AopdemoApplication.class, args);
+	}
+	@Bean
+	public CommandLineRunner commandLineRunner(AccountDAO accountDAO, MembershipDAO membershipDAO) {
+		return runner->{
+			demoTheBeforeAdvice(accountDAO,membershipDAO);
+		};
+	}
+
+	private void demoTheBeforeAdvice(AccountDAO accountDAO, MembershipDAO membershipDAO) {
+		Account newAccount= new Account("the main account","upper");
+		accountDAO.addAccount(newAccount,false);
+		accountDAO.doWork();
+		System.out.println(membershipDAO.addMember());
+
+		System.out.println(membershipDAO.countOfMemberships());
+	}
+}
---
+++ 10-spring-boot-apo/02-spring-boot-aop-pointcut-declarations/src/main/java/com/aopdemo/dao/AccountDAO.java
@@ -0,0 +1,9 @@
+package com.aopdemo.dao;
+
+
+import com.aopdemo.Account;
+
+public interface AccountDAO {
+    void addAccount(Account theAccount,boolean vipFlag);
+    boolean doWork();
+}
